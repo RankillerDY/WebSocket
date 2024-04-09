411e5c5d9f7484490fe3ed23638a7bf952292f43
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-04-09 18:18:51 +0700

    implement ChatController

---
+++ src/main/java/com/socket/WebSocket/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.socket.WebSocket.chat;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/socket/WebSocket/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.socket.WebSocket.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
